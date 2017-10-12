ecb198a709af18b7cbb793b489c255838931e958
Lucas de Oliveira Silva <lucasxm21@hotmail.com>
2017-10-12 12:41:34 -0300

    Sending messages

--- client.py
+++ client.py
@@ -1,30 +1,44 @@
 import json
+import struct
 import socket
 
 multicast_addr = '224.0.0.1'
 bind_addr = '0.0.0.0'
 port = 666
 
+send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+ttl = struct.pack('b', 1)
+send_sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+
+receive_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+membership = socket.inet_aton(multicast_addr) + socket.inet_aton(bind_addr)
+
+receive_sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, membership)
+receive_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+receive_sock.bind((bind_addr, port))
+
 
 def sendMsg(msg):
-    print(json.dumps(msg))
+    str_msg = json.dumps(msg)
+    send_sock.sendto(str_msg.encode('utf-8'), (multicast_addr, port))
 
 
 def changeUserName():
-    return input('Digite o seu usuário: ').replace(' ', '_')
+    return input('\nDigite o seu usuário: ').replace(' ', '_')
 
 
-message = {'user_name': changeUserName(), 'msg': ''}
+send_msg = {'user_name': changeUserName(), 'msg': ''}
 
 while True:
     cmd = input('\n->')
 
     if cmd == '/mu':
-        message['user_name'] = changeUserName()
+        send_msg['user_name'] = changeUserName()
     elif cmd == '/s':
         cmd = input('\nDeseja sair?\nQualquer coisa para calcelar ou [s/S] para sair: ')
         if cmd.lower() == 's':
             quit()
     else:
-        message['msg'] = cmd
-        sendMsg(message)
+        send_msg['msg'] = cmd
+        sendMsg(send_msg)
